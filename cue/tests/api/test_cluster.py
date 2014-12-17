0887839f44c5bc868da3a6c960b63f3537731c8b
dagnello <davide_agnello@hotmail.com>
2014-12-17 08:42:10 -0800

    Refactoring API - Cue Objects - DB modules
    * ensure autonomous DB transaction when creating a new cluster.
    * move DB read/write access calls from DB models to DB API.
    * add unit tests for DB models
    * add unit tests for DB API
    * update REST api to secod approach, documented here:
      https://wiki.openstack.org/wiki/Cue/api_2
    * add unit tests for REST API
    * add unit tests for Cue Objects
    * add integration tests for Cue Objects
    
    Change-Id: Ibbeaada7c24aa3e1e9c4cf1abaf60b3521a683f1

---
+++ cue/tests/api/test_cluster.py
@@ -0,0 +1,134 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Authors: Davide Agnello <davide.agnello@hp.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Copyright [2014] Hewlett-Packard Development Company, L.P.
+# limitations under the License.
+"""
+Tests for the API /cluster/ controller methods.
+"""
+
+import uuid
+
+from cue.db.sqlalchemy import models
+from cue import objects
+from cue.tests.api import api_common
+from cue.tests import utils as test_utils
+
+
+class TestGetCluster(api_common.ApiCommon):
+    def setUp(self):
+        super(TestGetCluster, self).setUp()
+
+    def test_get_cluster_not_found(self):
+        """test get non-existing cluster."""
+        data = self.get_json('/clusters/' + str(uuid.uuid4()),
+                             expect_errors=True)
+
+        self.assertEqual(404, data.status_code,
+                         'Invalid status code value received.')
+        self.assertEqual('404 Not Found', data.status,
+                         'Invalid status value received.')
+        self.assertIn('Cluster was not found',
+                      data.namespace["faultstring"],
+                      'Invalid faultstring received.')
+
+    def test_get_cluster_invalid_uuid_format(self):
+        """test get cluster with invalid id uuid format."""
+        invalid_uuid = u"25c06c22.fadd.4c83-a515-974a29668ba9"
+        data = self.get_json('/clusters/' + invalid_uuid, expect_errors=True)
+
+        self.assertEqual(400, data.status_code,
+                         'Invalid status code value received.')
+        self.assertEqual('400 Bad Request', data.status,
+                         'Invalid status value received.')
+        self.assertIn('badly formed cluster_id UUID string',
+                      data.namespace["faultstring"],
+                      'Invalid faultstring received.')
+
+    def test_get_cluster_invalid_uri(self):
+        """test get cluster with invalid URI string.
+
+        Example: get /clusters/<cluster_id>/invalid_resource
+        """
+
+    def test_get_cluster_valid_uri(self):
+        """test get cluster with valid URI strings.
+
+        Examples (with and without end forward slash):
+        get /clusters/<cluster_id>
+        get /clusters/<cluster_id>/
+        """
+
+    def test_get_cluster(self):
+        """test get cluster on valid existing cluster."""
+        cluster = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name)
+        data = self.get_json('/clusters/' + cluster.id)
+
+        self.validate_cluster_values(cluster, data)
+
+
+class TestDeleteCluster(api_common.ApiCommon):
+    def setUp(self):
+        super(TestDeleteCluster, self).setUp()
+
+    def test_delete_cluster_not_found(self):
+        """test delete non-existing cluster."""
+        data = self.delete('/clusters/' + str(uuid.uuid4()),
+                           expect_errors=True)
+
+        self.assertEqual(404, data.status_code,
+                         'Invalid status code value received.')
+        self.assertEqual('404 Not Found', data.status,
+                         'Invalid status value received.')
+        self.assertIn('Cluster was not found',
+                      data.namespace["faultstring"],
+                      'Invalid faultstring received.')
+
+    def test_delete_cluster_invalid_uuid_format(self):
+        """test delete cluster with invalid uuid format."""
+        invalid_uuid = u"25c06c22.fadd.4c83-a515-974a29668ba9"
+        data = self.delete('/clusters/' + invalid_uuid, expect_errors=True)
+
+        self.assertEqual(400, data.status_code,
+                         'Invalid status code value received.')
+        self.assertEqual('400 Bad Request', data.status,
+                         'Invalid status value received.')
+        self.assertIn('badly formed cluster_id UUID string',
+                      data.namespace["faultstring"],
+                      'Invalid faultstring received.')
+
+    def test_deleted_cluster_already_deleted(self):
+        """test delete cluster that has already been deleted."""
+
+    def test_delete_pending_cluster(self):
+        """test delete cluster that is pending deletion."""
+
+    def test_delete_cluster(self):
+        """test delete cluster on valid existing cluster."""
+        cluster = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name)
+        cluster_in_db = objects.Cluster.get_cluster_by_id(cluster.id)
+        self.assertEqual(models.Status.BUILDING, cluster_in_db.status,
+                         "Invalid cluster status value")
+        self.delete('/clusters/' + cluster.id)
+
+        cluster_in_db = objects.Cluster.get_cluster_by_id(cluster.id)
+        cluster.status = models.Status.DELETING
+        cluster.updated_at = cluster_in_db.created_at
+        cluster.updated_at = cluster_in_db.updated_at
+
+        data = self.get_json('/clusters/' + cluster.id)
+        self.validate_cluster_values(cluster, data)
---
+++ cue/tests/utils.py
@@ -0,0 +1,152 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Cue test utilities."""
+
+from oslo.utils import timeutils
+
+from cue.api.controllers import v1
+from cue.db.sqlalchemy import models
+from cue import objects
+
+
+def get_test_cluster(**kw):
+    return {
+        'id': kw.get('id', '1be26c0b-03f2-4d2e-ae87-c02d7f33c781'),
+        'project_id': kw.get('project_id', '1234567890'),
+        'name': kw.get('name', 'sample_cluster'),
+        'network_id': kw.get('network_id',
+                             '3dc26c0b-03f2-4d2e-ae87-c02d7f33c788'),
+        'status': kw.get('status', 'BUILDING'),
+        'flavor': kw.get('flavor', 'flavor1'),
+        'size': kw.get('size', 1),
+        'volume_size': kw.get('volume_size', 10),
+        'deleted': kw.get('deleted', False),
+        'created_at': kw.get('created_at', timeutils.utcnow()),
+        'updated_at': kw.get('updated_at', timeutils.utcnow()),
+        'deleted_at': kw.get('deleted_at', None),
+    }
+
+
+def create_api_test_cluster(**kw):
+    """Create test Cluster api object and return this object.
+
+    Function to be used to acquire an API Cluster object set with only required
+    fields.  This would mimic a cluster object values received from REST API.
+
+    :param kw: kwargs with overriding values for cluster's attributes.
+    :returns: Test Cluster API object.
+    """
+
+    cluster = get_test_cluster(**kw)
+
+    cluster_parameters = {
+        'name': cluster['name'],
+        'network_id': cluster['network_id'],
+        'flavor': cluster['flavor'],
+        'size': str(cluster['size']),
+        'volume_size': str(cluster['volume_size']),
+    }
+
+    new_cluster = v1.Cluster(**cluster_parameters)
+
+    return new_cluster
+
+
+def create_api_test_cluster_all(**kw):
+    """Create fully-populated test Cluster api object and return this object.
+
+    Function to be used to acquire an API Cluster object with all fields set.
+
+    :param kw: kwargs with overriding values for cluster's attributes.
+    :returns: Test Cluster API object.
+    """
+
+    cluster = get_test_cluster(**kw)
+
+    cluster_parameters = {
+        'name': cluster['name'],
+        'network_id': cluster['network_id'],
+        'flavor': cluster['flavor'],
+        'size': cluster['size'],
+        'volume_size': cluster['volume_size'],
+        'id': cluster['id'],
+        'project_id': cluster['project_id'],
+        'status': cluster['status'],
+        'created_at': cluster['created_at'],
+        'updated_at': cluster['updated_at'],
+    }
+
+    new_cluster = v1.Cluster(**cluster_parameters)
+
+    return new_cluster
+
+
+def create_db_test_cluster_from_objects_api(**kw):
+    """Create test Cluster entry in DB from objects API and return Cluster
+
+    DB object.  Function to be used to create test Cluster objects in the
+    database.
+
+    :param kw: kwargs with overriding values for cluster's attributes.
+    :returns: Test Cluster DB object.
+
+    """
+    cluster = get_test_cluster(**kw)
+
+    cluster_parameters = {
+        'name': cluster['name'],
+        'network_id': cluster['network_id'],
+        'flavor': cluster['flavor'],
+        'size': cluster['size'],
+        'volume_size': cluster['volume_size'],
+    }
+
+    new_cluster = objects.Cluster(**cluster_parameters)
+
+    project_id = cluster['project_id']
+
+    new_cluster.create(project_id)
+
+    return new_cluster
+
+
+def create_db_test_cluster_model_object(**kw):
+    """Create test Cluster DB model object.
+
+    :param kw: kwargs with overriding values for cluster's attributes.
+    :returns: Test Cluster DB model object.
+
+    """
+    cluster = get_test_cluster(**kw)
+
+    cluster_parameters = {
+        'name': cluster['name'],
+        'network_id': cluster['network_id'],
+        'flavor': cluster['flavor'],
+        'size': cluster['size'],
+        'volume_size': cluster['volume_size'],
+        'id': cluster['id'],
+        'project_id': cluster['project_id'],
+        'status': cluster['status'],
+        'deleted': cluster['deleted'],
+        'created_at': cluster['created_at'],
+        'updated_at': cluster['updated_at'],
+        'deleted_at': cluster['deleted_at'],
+    }
+
+    new_cluster = models.Cluster()
+    new_cluster.update(cluster_parameters)
+
+    return new_cluster
