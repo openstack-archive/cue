f84853021677a2ce1f7e2e3f41d9018eed0d9ce8
dagnello <davide_agnello@hotmail.com>
2014-12-17 08:42:10 -0800

    Refactoring API - Cue Objects - DB modules
    * ensure autonomous DB transaction when creating a new cluster.
    * move DB read/write access calls from DB models to DB API.
    * add unit tests for DB models
    * add unit tests for DB API
    * update REST api to secod approach, documented here:
      https://wiki.openstack.org/wiki/Cue/api_2
    * add initial set of REST API unit tests
    * add initial set of Cue Objects integration tests
    
    Change-Id: Ibbeaada7c24aa3e1e9c4cf1abaf60b3521a683f1

--- cue/tests/api/test_clusters.py
+++ cue/tests/api/test_clusters.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 #
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
 #
 #         http://www.apache.org/licenses/LICENSE-2.0
@@ -12,38 +12,98 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-Tests for the API /clusters/ methods.
+Tests for the API /clusters/ controller methods.
 """
+import anyjson
 
-import cue.tests.api as api_base
-from cue.tests.db import utils as dbutils
-
-
-# class TestClusterObject(base.TestCase):
-#
-#     def test_cluster_init(self):
-#         # port_dict = apiutils.port_post_data(node_id=None)
-#         # del port_dict['extra']
-#         # port = api_port.Port(**port_dict)
-#         # self.assertEqual(wtypes.Unset, port.extra)
+from cue.tests.api import api_common
+from cue.tests import utils as test_utils
 
 
-class TestListClusters(api_base.FunctionalTest):
-
-    cluster_name = "test-cluster"
-
+class TestListClusters(api_common.ApiCommon):
     def setUp(self):
         super(TestListClusters, self).setUp()
-        #self.cluster = dbutils.create_test_cluster()
 
     def test_empty(self):
         data = self.get_json('/clusters')
-        # TODO(vipul): This should probably return a empty 'clusters'
         self.assertEqual([], data)
 
     def test_one(self):
-        cluster = dbutils.create_test_cluster(name=self.cluster_name)
+        cluster = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name)
         data = self.get_json('/clusters')
 
-        self.assertEqual(cluster.id, data[0]["id"])
-        self.assertEqual(self.cluster_name, data[0]["name"])
+        self.assertEqual(len(data), 1, "Invalid number of clusters returned")
+
+        self.validate_cluster_values(cluster, data[0])
+
+    def test_multiple(self):
+        cluster_0 = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name + '_0')
+        cluster_1 = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name + '_1')
+        cluster_2 = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name + '_2')
+        cluster_3 = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name + '_3')
+        cluster_4 = test_utils.create_db_test_cluster_from_objects_api(
+            name=self.cluster_name + '_4')
+
+        data = self.get_json('/clusters')
+
+        self.assertEqual(len(data), 5, "Invalid number of clusters returned")
+
+        self.validate_cluster_values(cluster_0, data[0])
+        self.validate_cluster_values(cluster_1, data[1])
+        self.validate_cluster_values(cluster_2, data[2])
+        self.validate_cluster_values(cluster_3, data[3])
+        self.validate_cluster_values(cluster_4, data[4])
+
+
+class TestCreateCluster(api_common.ApiCommon):
+    def setUp(self):
+        super(TestCreateCluster, self).setUp()
+
+    def test_create_empty(self):
+        """test create an empty cluster."""
+        #TODO(dagnello): should this functionality be allowed or return error
+        #  inv params?
+
+    def test_create_size_one(self):
+        """test create a cluster with one node."""
+        api_cluster = test_utils.create_api_test_cluster(
+            name=self.cluster_name,
+            size=1)
+        cluster_dict = api_cluster.as_dict()
+        cluster_json = anyjson.serialize(cluster_dict)
+        header = {'Content-Type': 'application/json'}
+        data = self.post_json('/clusters', params=cluster_json, headers=header,
+                              expect_errors=True)
+        print(data)
+
+    def test_create_size_three(self):
+        """test create a cluster with three nodes."""
+
+    def test_create_size_five(self):
+        """test create a cluster with five nodes."""
+
+    def test_create_invalid_size(self):
+        """test with invalid size parameter."""
+
+    def test_create_invalid_volume_size(self):
+        """test with invalid volume_size parameter."""
+
+    def test_create_invalid_parameter_set_id(self):
+        """test with invalid parameter set: id."""
+
+    def test_create_invalid_parameter_set_status(self):
+        """test with invalid parameter set: status."""
+
+    def test_create_invalid_parameter_set_created_at(self):
+        """test with invalid parameter set: created_at."""
+
+    def test_create_invalid_parameter_set_updated_at(self):
+        """test with invalid parameter set: updated_at."""
+
+    def test_create_invalid_parameter_set_deleted_at(self):
+        """test with invalid parameter set: deleted_at."""
