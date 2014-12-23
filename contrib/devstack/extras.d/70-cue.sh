625435e1ce0ad9e82d7aee5840b92658e2eefc11
Endre Karlson <endre.karlson@hp.com>
2014-12-23 17:00:49 +0100

    Add Devstack integration
    Change-Id: I74f807e52fe1a7dd1521c3d1551b4fe0b17db4a3

---
+++ contrib/devstack/extras.d/70-cue.sh
@@ -0,0 +1,44 @@
+# check for service enabled
+if is_service_enabled cue; then
+
+    if [[ "$1" == "source" ]]; then
+        # Initial source of lib script
+        source $TOP_DIR/lib/cue
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Cue"
+        install_cue
+
+        echo_summary "Installing Cue Client"
+        install_cueclient
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        echo_summary "Configuring Cue"
+        configure_cue
+
+        if is_service_enabled key; then
+            echo_summary "Creating Cue Keystone Accounts"
+            create_cue_accounts
+        fi
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Initializing Cue"
+        init_cue
+
+        echo_summary "Starting Cue"
+        start_cue
+
+        echo_summary "Creating Initial Cue Resources"
+        create_cue_initial_resources
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        stop_cue
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        echo_summary "Cleaning Cue"
+        cleanup_cue
+    fi
+fi
