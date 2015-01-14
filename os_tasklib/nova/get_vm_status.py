912d1e077250ad4d58b54300c68042643bb2e4e6
Min Pae <sputnik13@gmail.com>
2015-01-14 14:23:32 -0800

    adding taskflow support with initial workflow
    - Adding taskflow support
    - Added initial workflow for create cluster
    
    Change-Id: I21ca51f50f8d983ab787c9435cfd0d576c3ed7a2

---
+++ os_tasklib/nova/get_vm_status.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+__author__ = 'sputnik13'
+
+import random
+
+import os_tasklib
+
+
+class GetVmStatus(os_tasklib.BaseTask):
+    def execute(self, vm_id, **kwargs):
+        #print(self.nova_client.servers.list())
+        print("Get VM Status for %s" % vm_id)
+        vm_status = random.choice(['BUILDING',
+                                   'ACTIVE',
+                                   'DELETED',
+                                   'SUSPENDED',
+                                   'PAUSED',
+                                   'ERROR',
+                                   'STOPPED'])
+        return vm_status
+
+    def revert(self, **kwargs):
+        print(kwargs)
