a5c29fba6f53320d87a9cebb8771f37fdbe616a7
Min Pae <sputnik13@gmail.com>
2015-01-14 14:23:32 -0800

    adding taskflow support with initial workflow
    - Adding taskflow support
    - Added initial workflow for create cluster
    
    Change-Id: I21ca51f50f8d983ab787c9435cfd0d576c3ed7a2

---
+++ os_tasklib/neutron/create_port.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+__author__ = 'sputnik13'
+
+import os_tasklib
+
+
+class CreatePort(os_tasklib.BaseTask):
+    default_provides = 'neutron_port_id'
+
+    def execute(self, **kwargs):
+        print("Create Neutron Port")
+        return "RANDOM_NEUTRON_PORT_ID"
+
+    def revert(self, **kwargs):
+        print("Delete Neutron Port %s" % kwargs['result'])
