f43264b6ae57ea3812e57dc75056751f1175b91e
Vipul Sabhaya <vipul.sabhaya@hp.com>
2015-01-27 15:58:45 -0800

    Adding Create Port Functionality for Cue Backend using TaskFlow
    * Create port uses neutron client
    * Added unit tests for new functionality
    Note:  Unit tests will fail unless neutron_client_parameters are
    updated in /cue/taskflow/os_utils/__init__.py for communication
    to an active neutron service (e.g. using devstack).
    
    Change-Id: Ia0f5d35d317726cd3ad22e4c5e1a34ae9eb65ddc

---
+++ cue/tests/taskflow/task/test_create_port.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+from cue import client
+from cue.tests import base
+import os_tasklib.neutron as neutron_task
+
+from neutronclient.common import exceptions
+from taskflow import engines
+from taskflow.patterns import linear_flow
+
+SHARED_CONF = {
+    'connection': 'zookeeper',
+}
+
+
+class CreatePortTests(base.TestCase):
+
+    task_store = {
+        'network_id': "0",
+        'port_name': "port_0",
+    }
+
+    def test_create_port_invalid_network(self):
+        # retrieve neutron client API class
+        neutron_client = client.neutron_client()
+
+        # create flow with "CreatePort" task
+        flow = linear_flow.Flow('create port').add(neutron_task.CreatePort(
+            os_client=neutron_client, provides='neutron_port_id'))
+
+        # generate a new UUID for an 'invalid' network_id
+        CreatePortTests.task_store['network_id'] = str(uuid.uuid4())
+
+        try:
+            engines.run(flow, store=CreatePortTests.task_store)
+        except exceptions.NetworkNotFoundClient:
+            """this exception is expected for this test"""
+        except Exception as e:
+            self.fail("Unexpected exception was thrown: " + e.message)
+        else:
+            self.fail("NetworkNotFoundClient exception not thrown as expected")
+
+    def test_create_port(self):
+        # retrieve neutron client API class
+        neutron_client = client.neutron_client()
+
+        # retrieve networks
+        network_list = neutron_client.list_networks()
+        self.assertNotEqual(len(network_list['networks']), 0,
+                            "No networks found")
+
+        # set a network_id and unique name to use
+        network_id = network_list['networks'][0]['id']
+        CreatePortTests.task_store['network_id'] = network_id
+        CreatePortTests.task_store['port_name'] = "port_" + str(uuid.uuid4())
+
+        # create flow with "CreatePort" task, given neutron client
+        flow = linear_flow.Flow('create port').add(neutron_task.CreatePort(
+            os_client=neutron_client, provides='neutron_port_id'))
+
+        # execute flow with parameters required for "CreatePort" task
+        engines.run(flow, store=CreatePortTests.task_store)
+
+        # retrieve list of ports from Neutron service
+        port_list = neutron_client.list_ports()
+
+        # find our newly created port
+        found = False
+        for port in port_list['ports']:
+            if port['network_id'] == CreatePortTests.task_store['network_id']:
+                if port['name'] == CreatePortTests.task_store['port_name']:
+                    found = True
+                    break
+
+        self.assertEqual(True, found, "New port was not found")
