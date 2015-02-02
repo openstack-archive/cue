e35d46615793abd3865538bf8700cd5d90868cbe
Min Pae <sputnik13@gmail.com>
2015-02-01 18:05:50 -0800

    add create_vm task
    - Add os_tasklib.nova.create_vm task for creating new VMs via nova
    - Modified nova and neutron fixtures to persist more information
      about networks, flavors, and images.
    
    Change-Id: I25a54eea44b6a24b46c98992d4699b5dd3455ca6

--- cue/tests/test_fixtures/nova.py
+++ cue/tests/test_fixtures/nova.py
@@ -5,7 +5,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -15,16 +15,55 @@
 
 import uuid
 
+import novaclient.exceptions as nova_exc
 import novaclient.v1_1.client as nova_client
 
+import cue.client as client
 import cue.tests.test_fixtures.base as base
 
 
-class VmDetails():
-    def __init__(self, vm_id, name, flavor):
+class VmDetails(object):
+    def __init__(self, vm_id, name, flavor, image):
         self.id = vm_id
         self.name = name
         self.flavor = flavor
+        self.image = image
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'flavor': self.flavor,
+        }
+
+
+class ImageDetails(object):
+    def __init__(self, created=None, human_id=None, id=None, minDisk=None,
+                 minRam=None, name=None, progress=None, status=None,
+                 updated=None):
+        self.created = created
+        self.human_id = human_id
+        self.id = id
+        self.minDisk = minDisk
+        self.minRam = minRam
+        self.name = name
+        self.progress = progress
+        self.status = status or 'ACTIVE'
+        self.updated = updated
+
+
+class FlavorDetails(object):
+    def __init__(self, disk=None, ephemeral=None, id=None, is_public=None,
+                 name=None, ram=None, rxtx_factor=None, swap=None, vcpus=None):
+        self.disk = disk or 10
+        self.ephemeral = ephemeral or 10
+        self.id = id or uuid.uuid4().hex
+        self.is_public = is_public or 'N/A'
+        self.name = name
+        self.ram = ram or 512
+        self.rxtx_factor = rxtx_factor or 1.0
+        self.swap = swap or 0
+        self.vcpus = vcpus or 2
 
 
 class NovaClient(base.BaseFixture):
@@ -34,32 +73,33 @@ class NovaClient(base.BaseFixture):
     connection in the absence of a working Neutron API endpoint.
     """
 
+    def __init__(self, *args, **kwargs):
+        super(NovaClient, self).__init__(*args, **kwargs)
+        self._vm_list = {}
+
+        image_detail = ImageDetails(name='cirros-0.3.2-x86_64-uec-kernel')
+        self._image_list = {
+            image_detail.id: image_detail
+        }
+
+        flavor_detail = FlavorDetails(name='m1.tiny')
+        self._flavor_list = {
+            flavor_detail.id: flavor_detail
+        }
+
     def setUp(self):
         """Set up test fixture and apply all method overrides."""
         super(NovaClient, self).setUp()
 
-        self._vm_list = {}
-        self._flavor_id = 1
-        self._image_id = 1
-        self._image_name = ""
-        self._flavor_name = ""
-
         v2_client = self.mock(nova_client.Client)
         v2_client.servers.create = self.create_vm
         v2_client.servers.delete = self.delete_vm
+        v2_client.servers.get = self.get_vm
         v2_client.servers.list = self.list_vms
+        v2_client.images.find = self.images_find
         v2_client.flavors.find = self.flavors_find
-        v2_client.images.find = self.find_image
 
-    def flavors_find(self, name=None):
-        self._flavor_name = name
-        return self._flavor_id
-
-    def find_image(self, name=None):
-        self._image_name = name
-        return self._image_id
-
-    def create_vm(self, **kwargs):
+    def create_vm(self, name, image, flavor, nics=None, **kwargs):
         """Mock'd version of novaclient...create_vm().
 
         Create a Nova VM.
@@ -68,30 +108,61 @@ class NovaClient(base.BaseFixture):
         :return: An updated copy of the 'body' that was passed in, with other
                  information populated.
         """
-        if 'name' in kwargs:
-            vm_name = kwargs['name']
-        else:
-            vm_name = "new_vm"
+        try:
+            flavor_id = flavor.id
+        except AttributeError:
+            flavor_id = flavor
 
-        if 'flavor' in kwargs:
-            vm_flavor = kwargs['flavor']
-        else:
-            vm_flavor = "medium"
+        try:
+            image_id = image.id
+        except AttributeError:
+            image_id = image
 
-        newVm = VmDetails(vm_id=uuid.uuid4(), name=vm_name, flavor=vm_flavor)
+        if not self._flavor_list.get(flavor_id):
+            raise nova_exc.BadRequest(404)
+
+        if not self._image_list.get(image_id):
+            raise nova_exc.BadRequest(404)
+
+        if nics is not None:
+            neutron_client = client.neutron_client()
+            for nic in nics:
+                if nic.get('net-id'):
+                    network_list = neutron_client.list_networks(
+                        id=nic['net-id'])
+                    if (not network_list or
+                            not network_list.get('networks') or
+                                len(network_list['networks']) == 0):
+                        raise nova_exc.BadRequest(404)
+
+        newVm = VmDetails(vm_id=uuid.uuid4(), name=name,
+                          flavor=flavor, image=image)
 
         self._vm_list[newVm.id] = newVm
 
         return newVm
 
-    def delete_vm(self, vm_id):
+    def delete_vm(self, server, **kwargs):
         """Mock'd version of novaclient...delete_vm().
 
         :param vm object with id instance variable
         :return: n/a
         """
-        if vm_id in self._vm_list:
-            del(self._vm_list[vm_id])
+        try:
+            server_id = server.id
+        except AttributeError:
+            server_id = server
+
+        try:
+            del (self._vm_list[server_id])
+        except KeyError:
+            pass
+
+    def get_vm(self, server, **kwargs):
+        try:
+            return self._vm_list[server.id]
+        except AttributeError:
+            return self._vm_list[server]
 
     def list_vms(self, retrieve_all=True, **_params):
         """Mock'd version of novaclient...list_vms().
@@ -102,3 +173,13 @@ class NovaClient(base.BaseFixture):
         """
         if retrieve_all:
             return self._vm_list.values()
+
+    def images_find(self, name, **kwargs):
+        for image_detail in self._image_list.itervalues():
+            if image_detail.name == name:
+                return image_detail
+
+    def flavors_find(self, name, **kwargs):
+        for flavor_detail in self._flavor_list.itervalues():
+            if flavor_detail.name == name:
+                return flavor_detail
--- os_tasklib/nova/create_vm.py
+++ os_tasklib/nova/create_vm.py
@@ -5,7 +5,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -14,17 +14,110 @@
 # under the License.
 
 
-__author__ = 'sputnik13'
-
 import os_tasklib
+
+from cue.common.i18n import _LW  # noqa
+from cue.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
 
 
 class CreateVm(os_tasklib.BaseTask):
-    def execute(self, neutron_port_id, cinder_volume_id, **kwargs):
-        #print self.os_client.servers.list()
-        print("Create VM and attach %s port and %s volume" %
-              (neutron_port_id, cinder_volume_id))
-        return "RANDOM_VM_ID"
+    """CreateVm Task
+
+    This task interfaces with Nova API and creates a VM based on parameters
+    provided to the Task.
+
+    """
+
+    def execute(self, name, image, flavor, meta=None, files=None,
+               reservation_id=None, min_count=None,
+               max_count=None, security_groups=None, userdata=None,
+               key_name=None, availability_zone=None,
+               block_device_mapping=None, block_device_mapping_v2=None,
+               nics=None, scheduler_hints=None,
+               config_drive=None, disk_config=None, tx_id=None, **kwargs):
+        """main execute method
+
+        :param name: Something to name the server.
+        :type name: string
+        :param image: UUID of VM Image to boot with.
+        :type image: string
+        :param flavor: UUID of VM flavor to boot onto.
+        :type flavor: string
+        :param meta: A dict of arbitrary key/value metadata to store for this
+                     server. A maximum of five entries is allowed, and both
+                     keys and values must be 255 characters or less.
+        :type meta: dict
+        :param files: A dict of files to overrwrite on the server upon boot.
+                      Keys are file names (i.e. ``/etc/passwd``) and values
+                      are the file contents (either as a string or as a
+                      file-like object). A maximum of five entries is allowed,
+                      and each file must be 10k or less.
+        :type files: dict
+        :param userdata: user data to pass to be exposed by the metadata
+                         server this can be a file type object as well or a
+                         string.
+        :type userdata: dict
+        :param reservation_id: a UUID for the set of servers being requested.
+        :type reservation_id: string
+        :param key_name: (optional extension) name of previously created
+                         keypair to inject into the instance.
+        :type key_name: string
+        :param availability_zone: Name of the availability zone for instance
+                                  placement.
+        :type availability_zone: string
+        :param block_device_mapping: (optional extension) A dict of block
+                                     device mappings for this server.
+        :type block_device_mapping: dict
+        :param block_device_mapping_v2: (optional extension) A dict of block
+                                        device mappings for this server.
+        :type block_device_mapping_v2: dict
+        :param nics:  (optional extension) an ordered list of nics to be
+                      added to this server, with information about
+                      connected networks, fixed IPs, port etc.
+        :type nics: list
+        :param scheduler_hints: (optional extension) arbitrary key-value pairs
+                                specified by the client to help boot an
+                                instance
+        :type scheduler_hints: dict
+        :param config_drive: (optional extension) value for config drive
+                             either boolean, or volume-id
+        :type config_drive: string
+        :param disk_config: (optional extension) control how the disk is
+                            partitioned when the server is created.  possible
+                            values are 'AUTO' or 'MANUAL'.
+        :type disk_config: string
+        :return: VM record provided by Nova
+        :rtype: dict
+        """
+
+        new_vm = self.os_client.servers.create(
+            name=name,
+            image=image,
+            flavor=flavor,
+            meta=meta,
+            files=files,
+            reservation_id=reservation_id,
+            min_count=min_count,
+            max_count=max_count,
+            security_groups=security_groups,
+            userdata=userdata,
+            key_name=key_name,
+            availability_zone=availability_zone,
+            block_device_mapping=block_device_mapping,
+            block_device_mapping_v2=block_device_mapping_v2,
+            nics=nics,
+            scheduler_hints=scheduler_hints,
+            config_drive=config_drive,
+            disk_config=disk_config
+        )
+        return new_vm.to_dict()
 
     def revert(self, **kwargs):
-        print("Delete VM %s" % kwargs['result'])
+        if kwargs.get('tx_id'):
+            LOG.warning(_LW("%(tx_id)s Create VM failed %(result)s") %
+                        {'tx_id': kwargs['tx_id'], 'result': kwargs['result']})
+        else:
+            LOG.warning(_LW("Create VM failed %s") % kwargs['result'])
