1f9ab9754cf60a3727413054edb54cc01cc430e0
Vipul Sabhaya <vipul.sabhaya@hp.com>
2015-02-23 13:16:22 -0800

    Initial port of RabbitMQ image elements
    * Installs rabbitmq-server
    * Templatized vesions of rabbitmq.config and erlang.cookie
    * Modify devstack scripts to build Cue Image
    
    Change-Id: I57ac73aaff0c936f3b4f1d63de0cf19266df8b1c

---
+++ contrib/image-elements/image-build-rabbitmq.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -eux
+
+# Path Settings
+export CUE_HOME="$(readlink -e $(dirname $(readlink -f $0))/../..)"
+
+# BUILD_DIR Directory where builds will be performed and images will be left
+export BUILD_DIR=${BUILD_DIR:-$CUE_HOME/build}
+
+# DIB Output Image Type
+export IMAGE_TYPE=${IMAGE_TYPE:-qcow2}
+
+# Image Name
+BUILD_FILE="rabbitmq-cue-image.qcow2"
+
+# Common elements we'll use in all builds
+COMMON_ELEMENTS=${COMMON_ELEMENTS:-"vm ubuntu"}
+
+# Common Settings for all msgaas images builds
+SIZE="2"
+ELEMENTS="os-apply-config os-refresh-config ntp hosts cue-rabbitmq-base ifmetric"
+ELEMENTS_PATH="$CUE_HOME/contrib/image-elements"
+
+# Install some required apt packages if needed
+if ! [ -e /usr/sbin/debootstrap -a -e /usr/bin/qemu-img ]; then
+  sudo apt-get update
+  sudo apt-get install --yes debootstrap qemu-utils
+fi
+ 
+
+if [ ! -d $BUILD_DIR/diskimage-builder ]; then
+  echo "---> Cloning diskimage-builder"
+  git clone https://git.openstack.org/openstack/diskimage-builder $BUILD_DIR/diskimage-builder
+fi
+
+if [ ! -d $BUILD_DIR/tripleo-image-elements ]; then
+  echo "---> Cloning tripleo-image-elements"
+  git clone https://git.openstack.org/openstack/tripleo-image-elements $BUILD_DIR/tripleo-image-elements
+fi
+
+# Setup the elements path
+export ELEMENTS_PATH="$ELEMENTS_PATH:$BUILD_DIR/tripleo-image-elements/elements:$BUILD_DIR/diskimage-builder/elements"
+
+# Prepare the build directory
+if [ ! -d $BUILD_DIR/dist ]; then
+  mkdir $BUILD_DIR/dist
+fi
+
+# Prepare venv for diskimage-builder
+virtualenv $BUILD_DIR/diskimage-builder/.venv
+
+# Build the image
+( set +u; . "$BUILD_DIR/diskimage-builder/.venv/bin/activate"; set -u;
+  pushd $BUILD_DIR/diskimage-builder
+  pip install -r requirements.txt
+  python setup.py install
+  popd
+  disk-image-create -a amd64 -o $BUILD_DIR/dist/$BUILD_FILE --image-size $SIZE $COMMON_ELEMENTS $ELEMENTS
+)
+
