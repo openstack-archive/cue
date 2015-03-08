5eae86514a58838f286301ce9611153791be39b0
Min Pae <sputnik13@gmail.com>
2015-03-08 09:53:05 -0700

    adding ubuntu image and openstack credentials
    - adding ubuntu image to devstack
    - adding openstack credentials to cue.conf
    - adding iptable NAT forwarding rules to allow devstack VMs to
      access WAN
    
    Change-Id: I6035440d5a73832e8af617aa3046a2854c08215a

---
+++ contrib/devstack/local.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+source $TOP_DIR/functions
+source $TOP_DIR/stackrc
+source $TOP_DIR/lib/cue
+DEST=${DEST:-/opt/stack}
+
+source $TOP_DIR/openrc admin admin
+
+IPTABLES_RULE='iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE'
+
+# Create NAT rule to allow VMs to NAT to host IP
+if [[ -z $(sudo iptables -t nat -L | grep MASQUERADE | tr -d ' ' | grep anywhereanywhere) ]]; then
+    sudo $IPTABLES_RULE
+fi
+
+# Make VM NAT rule persistent
+# TODO(sputnik13): this should ideally be somewhere other than /etc/rc.local
+if [[ -z $(grep "$IPTABLES_RULE" /etc/rc.local) ]]; then
+    sudo sed -i -e "s/^exit 0/$IPTABLES_RULE\nexit 0/" /etc/rc.local
+fi
+
+if [[ ! -x /etc/rc.local ]]; then
+    sudo chmod +x /etc/rc.local
+fi
+
+# Generate an ssh keypair to add to devstack
+if [[ ! -f ~/.ssh/id_rsa ]]; then
+    ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
+fi
+
+# Add ssh keypair to admin account
+if [[ -z $(nova keypair-list | grep vagrant) ]]; then
+    nova keypair-add --pub-key ~/.ssh/id_rsa.pub vagrant
+fi
+
+# Add ping and ssh rules to rabbitmq security group
+neutron security-group-rule-create --direction ingress --protocol icmp --remote-ip-prefix 0.0.0.0/0 $CUE_RABBIT_SECURITY_GROUP
+neutron security-group-rule-create --direction ingress --protocol tcp --port-range-min 22 --port-range-max 22 --remote-ip-prefix 0.0.0.0/0 $CUE_RABBIT_SECURITY_GROUP
+
+# Add static nameserver to private-subnet
+neutron subnet-update --dns-nameserver 8.8.8.8 private-subnet
+
+# Add ssh keypair to demo account
+source $TOP_DIR/openrc demo demo
+if [[ -z $(nova keypair-list | grep vagrant) ]]; then
+    nova keypair-add --pub-key ~/.ssh/id_rsa.pub vagrant
+fi
+
