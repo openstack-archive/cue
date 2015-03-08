96352937d597698cdfdaa7e243a159e948f61ff4
Min Pae <sputnik13@gmail.com>
2015-03-08 09:53:05 -0700

    adding ubuntu image and openstack credentials
    - adding ubuntu image to devstack
    - adding openstack credentials to cue.conf
    - adding iptable NAT forwarding rules to allow devstack VMs to
      access WAN
    
    Change-Id: I6035440d5a73832e8af617aa3046a2854c08215a

--- contrib/vagrant/lib/devstack_script.rb
+++ contrib/vagrant/lib/devstack_script.rb
@@ -14,7 +14,7 @@ EOF
 
 test -d devstack || git clone https://git.openstack.org/openstack-dev/devstack
 
-rsync -av --exclude='.tox' --exclude='.venv' --exclude='.vagrant' --exclude='contrib/vagrant' /home/vagrant/cue /opt/stack
+rsync -av --exclude='.tox' --exclude='.venv' --exclude='.vagrant' /home/vagrant/cue /opt/stack
 
 if [ -f "/home/vagrant/python-cueclient" ]; then
     rsync -av --exclude='.tox' --exclude='.venv' --exclude='.vagrant' --exclude='contrib/vagrant' /home/vagrant/python-cueclient /opt/stack
@@ -24,6 +24,12 @@ fi
 if [ ! -f "/home/vagrant/devstack/local.conf" ]; then
     cp /opt/stack/cue/contrib/devstack/local.conf /home/vagrant/devstack/local.conf
 fi
+
+# Install Vagrant local.sh sample
+if [ ! -f "/home/vagrant/devstack/local.sh" ]; then
+    cp /opt/stack/cue/contrib/devstack/local.sh /home/vagrant/devstack/local.sh
+fi
+
 
 for f in extras.d/* lib/*; do
     if [ ! -f "/home/vagrant/devstack/$f" ]; then
