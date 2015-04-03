8f8aef1d5b390b0d308495d86fe908609671ea62
Abitha Palaniappan <abitha.palaniappan@hp.com>
2015-04-03 13:55:25 -0700

    Broker image selection
    - Adding revised alembic version with broker and broker metadata tables
    - Adding Broker and BrokerMetadata classes to objects and models
    - Adding crud functionalities for broker image selection
    - Modifying devstack script to set default broker and image
    
    Change-Id: I6f76d642d9ea3c201935238086c3008cfb00b2fb

--- cue/tests/base.py
+++ cue/tests/base.py
@@ -29,6 +29,8 @@ from cue.common import context as cue_context
 from cue.db.sqlalchemy import api as db_api
 from cue.manage import database
 from cue.tests.fixtures import database as database_fixture
+from cue import objects
+
 
 from oslo.config import cfg
 from oslotest import base
@@ -69,6 +71,27 @@ class TestCase(base.BaseTestCase):
             for fixture_cls in self.additional_fixtures:
                 fixture = fixture_cls()
                 self.useFixture(fixture)
+
+        self.set_broker()
+
+    def set_broker(self):
+        # Loading default broker and image to broker and brokerMetadata table
+        broker_values = {
+            'name': 'rabbitmq',
+            'active': '1',
+        }
+        broker = objects.Broker(**broker_values)
+        broker.create_broker(None)
+        broker_list = broker.get_brokers(None)
+        broker_id = broker_list[0]['id']
+
+        metadata_value = {
+            'key': 'IMAGE',
+            'value': 'f7e8c49b-7d1e-472f-a78b-7c46a39c85be',
+            'broker_id': broker_id
+        }
+        metadata = objects.BrokerMetadata(**metadata_value)
+        metadata.create_broker_metadata(None)
 
     def get_context(self, **kwargs):
         auth_token = kwargs.get('auth_token', "auth_xxx")
