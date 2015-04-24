8c35bef1007fef9cb1fc231fd603db6bea77bcb0
Daniel Allegood <daniel.allegood@hp.com>
2015-04-24 15:11:33 -0700

    cue-manage broker unit tests
    Unit tests for each of the cue-manage broker api functions.
    
    Change-Id: Ibd0fd12b439e971fb123ea50970604c3123a78f9

--- cue/tests/functional/db/test_api.py
+++ cue/tests/functional/db/test_api.py
@@ -19,6 +19,9 @@
 
 import uuid
 
+from oslo_db import exception as oslo_exception
+
+from cue.common import exception
 from cue.db import api as db_api
 from cue.tests.functional import base
 
@@ -88,3 +91,215 @@ class ApiTests(base.FunctionalTestCase):
 
     def test_update_cluster_deleting(self):
         """Verifies create cluster DB API."""
+
+    def test_create_broker(self):
+        """Verifies create broker DB API."""
+
+        broker_values = {
+            "name": "Test Broker Name",
+            "active": 1
+        }
+
+        db_broker = self.dbapi.create_broker(self.context, broker_values)
+
+        self.assertEqual(broker_values['name'], db_broker.name,
+                         "invalid broker name value")
+        self.assertEqual(broker_values['active'], db_broker.active,
+                         "invalid broker active value")
+
+    def test_get_default_broker(self):
+        """Verifies get brokers DB API."""
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIsNotNone(brokers,
+                             "Should have found the default RabbitMQ broker.")
+        self.assertIs(len(brokers),
+                      1,
+                      "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+        self.assertEqual(brokers[0].name,
+                         "rabbitmq",
+                         "Should have found the default rabbitmq broker, "
+                         "instead found: " + str(brokers[0].name))
+
+    def test_get_multiple_brokers(self):
+        """Verifies get brokers DB API."""
+
+        self.dbapi.create_broker(self.context,
+            {
+                "name": "Test Broker Name 1",
+                "active": 1
+            })
+        self.dbapi.create_broker(self.context,
+            {
+                "name": "Test Broker Name 2",
+                "active": 1
+            })
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIsNotNone(brokers,
+                             "Should have found the default RabbitMQ broker.")
+        self.assertIs(len(brokers),
+                      3,
+                      "Should have found 3 brokers, instead found " +
+                      str(len(brokers)))
+
+    def test_delete_broker(self):
+        """Verifies delete broker DB API."""
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      1,
+                      "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+        broker_id = brokers[0].id
+
+        self.dbapi.delete_broker(self.context, broker_id)
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      0,
+                      "Should have found 0 brokers after deleting the "
+                      "default broker. Instead found " + str(len(brokers)))
+
+    def test_update_broker(self):
+        """Verifies update broker DB API."""
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      1,
+                      "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+        broker_id = brokers[0].id
+
+        self.assertEqual(brokers[0].name,
+                         "rabbitmq",
+                         "Should have found the default rabbitmq broker.  "
+                         "Instead found: " + brokers[0].name)
+
+        broker_values = {
+            "name": "Test Broker Name",
+            "active": 1
+        }
+
+        self.dbapi.update_broker(self.context, broker_id, broker_values)
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      1, "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+
+        self.assertEqual(brokers[0].name,
+                         "Test Broker Name",
+                         "Should have found the default rabbitmq broker.  "
+                         "Instead found: " + brokers[0].name)
+
+    def test_create_broker_metadata(self):
+        """Verifies create broker metadata DB API."""
+
+        broker_values = {
+            "name": "Test Broker Name",
+            "active": 1
+        }
+
+        db_broker = self.dbapi.create_broker(self.context, broker_values)
+        broker_metadata_values = {
+            "broker_id": db_broker.id,
+            "key": "IMAGE",
+            "value": "test_image"
+        }
+        db_broker = self.dbapi.create_broker_metadata(self.context,
+                                                      broker_metadata_values)
+
+        self.assertEqual(broker_metadata_values['broker_id'],
+                         db_broker.broker_id,
+                         "invalid broker_metadata ID")
+        self.assertEqual(broker_metadata_values['key'], db_broker.key,
+                         "invalid broker key")
+        self.assertEqual(broker_metadata_values['value'], db_broker.value,
+                         "invalid broker value")
+
+        # Testing broker ID that should not exist in the table
+        broker_metadata_values['broker_id'] = UUID1
+        try:
+            self.assertRaises(exception.NotFound,
+                              self.dbapi.create_broker_metadata,
+                              self.context,
+                              broker_metadata_values)
+        except Exception as err:
+            self.assertIsNotNone(None,
+                                 "Unhandled exception from "
+                                 "create_broker_metadata with UUI that isn't "
+                                 "in the broker table: " + str(err))
+
+        # Testing with non-UUID formatted ID
+        broker_metadata_values['broker_id'] = "Not a UUID"
+        try:
+            self.assertRaises(exception.Invalid,
+                              self.dbapi.create_broker_metadata,
+                              self.context,
+                              broker_metadata_values)
+        except Exception as err:
+            self.assertIsNotNone(None,
+                                 "Unhandled exception from "
+                                 "create_broker_metadata with invalid UUID: "
+                                 + str(err))
+
+    def test_get_broker_metadata_by_broker_id(self):
+        """Verifies get broker metadata by broker id DB API."""
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      1,
+                      "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+        broker_id = brokers[0].id
+        broker_metadata = self.dbapi.get_broker_metadata_by_broker_id(
+            self.context,
+            broker_id)
+
+        self.assertIs(len(broker_metadata),
+                      1,
+                      "Should have found the default rabbitmq broker ID.")
+
+        # TODO(team) May want to have all cue.db.sqlalchemy.api.Connection
+        # TODO(team) functions that take a broker_id assert it is of type UUID
+        self.assertRaises(oslo_exception.DBError,
+                          self.dbapi.get_broker_metadata_by_broker_id,
+                          self.context,
+                          "Bad ID")
+
+    def test_delete_broker_metadata(self):
+        """Verifies delete broker metadata DB API."""
+
+        brokers = self.dbapi.get_brokers(self.context)
+        self.assertIs(len(brokers),
+                      1,
+                      "Should have found only 1 broker, instead found " +
+                      str(len(brokers)))
+        broker_id = brokers[0].id
+        broker_metadata = self.dbapi.get_broker_metadata_by_broker_id(
+            self.context,
+            broker_id)
+
+        self.dbapi.delete_broker_metadata(self.context, broker_metadata[0].id)
+
+        broker_metadata = self.dbapi.get_broker_metadata_by_broker_id(
+            self.context,
+            broker_id)
+
+        self.assertIs(0, len(broker_metadata), "Broker metadata should be "
+                                               "empty after the delete.")
+
+    def test_get_image_id_by_broker_name(self):
+
+        default_metadata_id = "f7e8c49b-7d1e-472f-a78b-7c46a39c85be"
+        image_id = self.dbapi.get_image_id_by_broker_name(self.context,
+                                                          'rabbitmq')
+
+        self.assertEqual(default_metadata_id, image_id,
+                      "Should have found the image ID of the default rabbitmq"
+                      " broker.")
+        self.assertRaises(exception.NotFound,
+                          self.dbapi.get_image_id_by_broker_name,
+                          self.context,
+                          "fake mq")
