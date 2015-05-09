d003d0ff4281352c64a62e492b3f96259a00f79c
Steve Leon <kokhang@gmail.com>
2015-05-08 18:45:42 -0700

    Updating API Context hook header data extraction for context
    Updating hooks to conform to updated keystone middleware
    headers.  Project/Tenant name was being persisted in DB instead
    of project/tenant ID.
    
    closes-bug: 1454362
    
    Change-Id: I3c757d123e7b8369769cd1f49672a5c07fe01ec5

--- cue/api/hooks.py
+++ cue/api/hooks.py
@@ -63,9 +63,8 @@ class ContextHook(hooks.PecanHook):
 
     def before(self, state):
         user_id = state.request.headers.get('X-User-Id')
-        user_id = state.request.headers.get('X-User', user_id)
-        tenant = state.request.headers.get('X-Tenant-Id')
-        tenant = state.request.headers.get('X-Tenant', tenant)
+        user_name = state.request.headers.get('X-User-Name', user_id)
+        tenant_id = state.request.headers.get('X-Project-Id')
         domain_id = state.request.headers.get('X-User-Domain-Id')
         domain_name = state.request.headers.get('X-User-Domain-Name')
         auth_token = state.request.headers.get('X-Auth-Token')
@@ -74,8 +73,8 @@ class ContextHook(hooks.PecanHook):
 
         state.request.context = context.RequestContext(
             auth_token=auth_token,
-            user=user_id,
-            tenant=tenant,
+            user=user_name,
+            tenant=tenant_id,
             domain_id=domain_id,
             domain_name=domain_name,
             is_public_api=is_public_api)
