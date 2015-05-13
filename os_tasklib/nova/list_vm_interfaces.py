0acff201e7366b1ad2ff6d2968bdd40e76201f0b
dagnello <davide_agnello@hotmail.com>
2015-05-13 15:05:11 -0700

    Deleting VM ports on cluster delete flow
    Change-Id: Ib311b23fdf3a58bc4b79257ed412e9d53d039013
    closes-bug: 1448140

---
+++ os_tasklib/nova/list_vm_interfaces.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os_tasklib
+
+from cue.common.i18n import _LW  # noqa
+
+import novaclient.exceptions as nova_exc
+from oslo_log import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ListVmInterfaces(os_tasklib.BaseTask):
+    """ListVmInterfaces Task
+
+    This task interfaces with Nova API and retrieves the list of interfaces
+    attached to the indicated VM.
+
+    """
+    def execute(self, server, **kwargs):
+        """Main execute method
+
+        :param server: vm id to get list of attached interfaces to
+        :type server: string
+        :return: list of interfaces
+        """
+        interfaces = self.os_client.servers.interface_list(server=server)
+        return interfaces
