77b5a0a1d010a40d7288605515163c522502022f
Steve Leon <kokhang@gmail.com>
2015-05-13 09:45:51 -0700

    Testing Gate. Do not merge
    Change-Id: Ie5ffaf169d3915c4bd73d61ab05bfa8ac3f7f36b

--- tests/gate_hook.sh
+++ tests/gate_hook.sh
@@ -4,7 +4,8 @@ set -ex
 
 pushd $BASE/new/devstack
 
-export ENABLED_SERVICES=cue,cue-api,cue-worker,zookeeper,dib,n-novnc
+export ENABLED_SERVICES=cue,cue-api,cue-worker,zookeeper,dib
+export PROJECTS="stackforge/cue-dashboard openstack/diskimage-builder $PROJECTS"
 
 echo "DEST=/opt/stack/new" >> $BASE/new/devstack/localrc
 echo "CUE_MANAGEMENT_KEY=cue-mgmt-key" >> $BASE/new/devstack/localrc
