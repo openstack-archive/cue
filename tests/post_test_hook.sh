9c95590ca046495104735f0997c602184be46a93
Steve Leon <kokhang@gmail.com>
2015-05-13 09:45:51 -0700

    Testing Gate. Do not merge
    Change-Id: Ie5ffaf169d3915c4bd73d61ab05bfa8ac3f7f36b

--- tests/post_test_hook.sh
+++ tests/post_test_hook.sh
@@ -3,6 +3,6 @@
 set -ex
 
 cd /opt/stack/new/cue/tests/integration
-
+cp /opt/stack/new/cue/tests/integration/cue-integration.conf /etc/cue/cue-integration.conf
 # Run the Cue Tempest tests
 sudo ./run_tests.sh
