21fec652e7f50b8ab0ed04a5e6152e9e74b34ace
Min Pae <sputnik13@gmail.com>
2015-06-10 02:17:10 -0700

    Fix revert on ERROR/Exception
    Failure on a sub flow that is being Retry'd is not forcing the
    surrounding flow to also REVERT.  This is because the Retry controller
    is returning REVERT rather than REVERT_ALL.
    
    Change-Id: Ib70fd37a0349c5cf89f73a2ca2334400b87bff56
    Closes-Bug: 1452958

--- cue/taskflow/flow/create_cluster_node.py
+++ cue/taskflow/flow/create_cluster_node.py
@@ -161,7 +161,7 @@ def create_cluster_node(cluster_id, node_number, node_id, graph_flow,
     #todo(dagnello): make retry times configurable
     check_rabbit_online = linear_flow.Flow(
         name="wait for RabbitMQ ready state %s" % node_name,
-        retry=retry.Times(node_check_max_count))
+        retry=retry.Times(node_check_max_count, revert_all=True))
     check_rabbit_online.add(
         os_common.VerifyNetwork(
             name="get RabbitMQ status %s" % node_name,
