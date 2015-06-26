bf331e1afa17e54d541e845c83f3d2f88f8231cf
Sergey Skripnick <sskripnick@mirantis.com>
2015-06-26 13:41:35 +0300

    Do not override ENABLED_SERVICES in gate_hook
    This variable should not be overridden, otherwise it is impossible
    to enable other services on cue gates.
    
    Change-Id: Id4a1af22f46456227b7634c2c187aeebebf155e1

--- tests/gate_hook.sh
+++ tests/gate_hook.sh
@@ -5,8 +5,8 @@ set -ex
 pushd $BASE/new/devstack
 
 export KEEP_LOCALRC=1
-export ENABLED_SERVICES=cue,cue-api,cue-worker,zookeeper,dib
-export PROJECTS="openstack/diskimage-builder openstack/cue-dashboard $PROJECTS"
+export ENABLED_SERVICES="cue,cue-api,cue-worker,zookeeper,dib,$ENABLED_SERVICES"
+export PROJECTS="openstack/diskimage-builder stackforge/cue-dashboard $PROJECTS"
 export DEVSTACK_GATE_NEUTRON=1
 
 echo "CUE_MANAGEMENT_KEY=cue-mgmt-key" >> $BASE/new/devstack/localrc
