1379b573bd9660d35234622e3ad1e0bebe17acfe
Daniel Allegood <daniel.allegood@hp.com>
2015-07-27 14:18:11 -0700

    Check RabbitMQ cluster status via REST API during cluster create
    This is replacing the TaskFlow task to check when a rabbit node begins to
    respond to port 5672.  We now use rabbits management plugin and REST API
    to check when the cluster is active and able to consume jobs. The new task
    is added at the end of the create_cluster flow.  This is (arbitrarily) going
    to the 0th node's RabbitMQ Management port.
    
    Change-Id: I9dffdefd9cdcd6317f5c3114c59b4b30b8218239

---
+++ cue/taskflow/task/get_rabbit_cluster_status.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import time
+import urllib
+import urllib2
+
+import taskflow.task
+
+
+class GetRabbitClusterStatus(taskflow.task.Task):
+    def __init__(self,
+                 retry_delay_seconds=None,
+                 retry_delay_ms=None,
+                 name=None,
+                 **kwargs):
+        """Constructor
+
+        This constructor sets the retry delay for this task's revert method.
+
+        :param retry_delay_seconds: retry delay in seconds
+        :param retry_delay_ms: retry delay in milliseconds
+        :param name: unique name for atom
+        """
+        super(GetRabbitClusterStatus, self).__init__(name=name, **kwargs)
+
+        self.sleep_time = 0
+        if retry_delay_seconds:
+            self.sleep_time = retry_delay_seconds
+
+        if retry_delay_ms:
+            self.sleep_time += retry_delay_ms / 1000.0
+
+    def execute(self,
+                vm_ip,
+                default_rabbit_user,
+                default_rabbit_pass,
+                proto=None,
+                **kwargs):
+        """Main execute method to verify Rabbitmq clustering in a VM
+
+        :param vm_ip: VM ip address
+        :type vm_ip: string
+        :param default_rabbit_user: Username for RabbitMQ
+        :type default_rabbit_user: string
+        :param default_rabbit_pass: Password for RabbitMQ
+        :type default_rabbit_pass: string
+        :param proto: Protocol for url, http/https
+        :type proto: string
+        """
+        if not proto:
+            proto = 'http'
+
+        base_url = proto + '://' + vm_ip + ':15672/api'
+
+        # RMQ management url to get a list of the existing vhosts
+        vhosts_url = base_url + '/vhosts'
+        # RMQ management url to query whether the RMQ nodes have clustered
+        aliveness_url = base_url + '/aliveness-test'
+
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None,
+                                  vhosts_url,
+                                  default_rabbit_user,
+                                  default_rabbit_pass)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        res = opener.open(vhosts_url)
+
+        json_res = json.load(res)
+
+        result = {}
+        for x in json_res:
+            vhost = urllib.quote(x['name'], '')
+            cur_aliveness_url = aliveness_url + '/' + vhost
+
+            password_mgr.add_password(None,
+                                      cur_aliveness_url,
+                                      default_rabbit_user,
+                                      default_rabbit_pass)
+            handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+            opener = urllib2.build_opener(handler)
+            res = opener.open(cur_aliveness_url)
+
+            result[x['name']] = json.load(res)['status']
+        if all(val == 'ok' for val in result.values()):
+            return "ACTIVE"
+        return "BUILDING"
+
+    def revert(self, *args, **kwargs):
+        """Revert GetRabbitClusterStatus
+
+        This method is executed upon failure of the GetRabbitClusterStatus or
+        the Flow that the Task is part of.
+
+        :param args: positional arguments that the task required to execute.
+        :type args: list
+        :param kwargs: keyword arguments that the task required to execute; the
+                       special key `result` will contain the :meth:`execute`
+                       results (if any) and the special key `flow_failures`
+                       will contain any failure information.
+        """
+        if self.sleep_time > 0:
+            time.sleep(self.sleep_time)
