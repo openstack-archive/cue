2810da2ba1c1da0d39ddd0a9f1da80d6e196d13e
Daniel Allegood <daniel.allegood@hp.com>
2015-07-27 14:18:11 -0700

    IGNORE - Check RabbitMQ cluster status via REST API during cluster create
    This is replacing the TaskFlow task to check when a rabbit node begins to
    respond to port 5672.  We now use rabbits management plugin and REST API
    to check when the cluster is active and able to consume jobs. The new task
    is added at the end of the create_cluster flow.  This is (arbitrarily) going
    to the 0th node's RabbitMQ Management port.
    
    Change-Id: I9dffdefd9cdcd6317f5c3114c59b4b30b8218239

---
+++ cue/tests/functional/fixtures/urllib2.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import StringIO
+import urllib2
+
+import cue.tests.functional.fixtures.base as base
+
+
+class Urllib2ResultDetails(object):
+    urllib2_result_list = []
+
+    @staticmethod
+    def set_urllib2_result(results):
+        """Helper function to setup sequence of fixed results.
+
+        :param results: list of results
+        """
+        for result in results:
+            io = StringIO.StringIO(result)
+            Urllib2ResultDetails.urllib2_result_list.append(io)
+
+    @staticmethod
+    def get_urllib2_result():
+        """Returns the next result in configured sequence.
+
+        If result sequence is empty, an empty string result of '' is returned.
+
+        :return: Current urllib result in urllib2_result_list.
+        """
+        if len(Urllib2ResultDetails.urllib2_result_list) == 0:
+            result = ''
+        else:
+            result = Urllib2ResultDetails.urllib2_result_list.pop()
+        return result
+
+
+class Urllib2Fixture(base.BaseFixture):
+    """A test fixture to simulate urllib2 calls
+
+    This class is used in test cases to simulate urllib2 calls.
+    """
+    def __init__(self, *args, **kwargs):
+        super(Urllib2Fixture, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """Set up test fixture and apply all method overrides."""
+        super(Urllib2Fixture, self).setUp()
+
+        urllib2_client = self.mock('urllib2.OpenerDirector')
+        urllib2_client.open = self.open
+
+    def open(self, url):
+        result = Urllib2ResultDetails.get_urllib2_result()
+        if result is 'HTTPError':
+            raise urllib2.HTTPError
+        elif result is 'URLError':
+            raise urllib2.URLError
+        return result
--- tests/integration/api/v1/clusters/test_clusters.py
+++ tests/integration/api/v1/clusters/test_clusters.py
@@ -73,6 +73,14 @@ class ClusterTest(tempest_lib.base.BaseTestCase):
             if time.time() - start_time > 1800:
                 self.get_logs(cluster_resp['id'])
                 self.fail('Waited 30 minutes for cluster to get ACTIVE')
+        if cluster_resp['status'] != 'ACTIVE':
+            self.get_logs(cluster_resp['id'])
+
+        # Temporary debug
+        import subprocess
+        out = subprocess.check_output(['netstat', '-an'])
+        print "netstat findme\n" + str(out)
+
         self.assertEqual(cluster_resp['status'], 'ACTIVE',
                          'Create cluster failed')
 
@@ -110,6 +118,7 @@ class ClusterTest(tempest_lib.base.BaseTestCase):
 
     @staticmethod
     def get_logs(cluster_id=None):
+        ip = None
         admin_client = client.ServerClient()
         nodes = admin_client.get_cluster_nodes(cluster_id)
         for node in nodes['servers']:
