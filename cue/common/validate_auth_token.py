eae934989fb10ee75ab43642396278e390afe9d9
dagnello <dagnello@hp.com>
2015-08-21 11:04:28 -0700

    Adding authentication support to API
    Authentication field is used to provide broker credentials in
    Cue create cluster requests.
    
    Change-Id: Icfeac617e82a4213277209990551120b8caf8530

---
+++ cue/common/validate_auth_token.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from cue.common.i18n import _LE  # noqa
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+
+
+class AuthTokenValidator(object):
+
+    PLAIN_AUTH = "PLAIN"
+    RABBIT_BROKER = "RABBIT"
+
+    @staticmethod
+    def validate_token(auth_type, broker_type, token):
+        auth_validator = None
+
+        if auth_type.upper() == AuthTokenValidator.PLAIN_AUTH:
+            if broker_type.upper() == AuthTokenValidator.RABBIT_BROKER:
+                auth_validator = RabbitPlainAuthTokenValidator(token=token)
+            else:
+                LOG.error(_LE('Invalid broker type received: %(broker_type)'),
+                         ({"broker_type": broker_type}))
+        else:
+            LOG.error(_LE('Invalid authentication type received: '
+                          '%(auth_type)'), ({"auth_type": auth_type}))
+
+        return auth_validator
+
+    def validate(self):
+        return True
+
+
+class RabbitPlainAuthTokenValidator(AuthTokenValidator):
+    MIN_USERNAME_LENGTH = 1
+    MAX_USERNAME_LENGTH = 255
+    MIN_PASSWORD_LENGTH = 1
+    MAX_PASSWORD_LENGTH = 255
+
+    def __init__(self, token):
+        self.token = token
+
+    def validate(self):
+        valid_username = False
+        valid_password = False
+
+        if self.token is not None:
+            if 'username' in self.token:
+                if ((len(self.token['username']) >=
+                        RabbitPlainAuthTokenValidator.MIN_USERNAME_LENGTH) and
+                    (len(self.token['username']) <=
+                     RabbitPlainAuthTokenValidator.MAX_USERNAME_LENGTH)):
+                    valid_username = True
+                else:
+                    LOG.error(_LE('Invalid username: %(username)'), (
+                        {'username': self.token['username']}))
+            if 'password' in self.token:
+                if ((len(self.token['password']) >=
+                        RabbitPlainAuthTokenValidator.MIN_PASSWORD_LENGTH) and
+                    (len(self.token['password']) <=
+                     RabbitPlainAuthTokenValidator.MAX_PASSWORD_LENGTH)):
+                    valid_password = True
+                else:
+                    LOG.error(_LE('Invalid username: %(password)'), (
+                        {'password': self.token['password']}))
+
+        return valid_username and valid_password
---
+++ cue/tests/unit/common/test_validate_auth_token.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-2015 Hewlett-Packard Development Company, L.P.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from cue.common import validate_auth_token as auth_validate
+from cue.tests.unit import base
+
+
+class TestValidateAuthToken(base.UnitTestCase):
+
+    def test_auth_token_validator_base(self):
+        validator = auth_validate.AuthTokenValidator()
+        valid = validator.validate()
+        self.assertEqual(valid, True)
+
+    def test_auth_token_rabbit_validator(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='rabbit',
+            token={'username': 'rabbitmq',
+                   'password': 'rabbit'})
+        valid = validator.validate()
+        self.assertEqual(valid, True)
+
+    def test_auth_token_invalid_broker(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='blah',
+            token={'username': 'rabbitmq',
+                   'password': 'rabbit'})
+        self.assertEqual(validator, None)
+
+    def test_auth_token_invalid_type(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='blah', broker_type='rabbit',
+            token={'username': 'rabbitmq',
+                   'password': 'rabbit'})
+        self.assertEqual(validator, None)
+
+    def test_auth_rabbit_token_missing_username(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='rabbit',
+            token={'password': 'rabbit'})
+        valid = validator.validate()
+        self.assertEqual(valid, False)
+
+    def test_auth_rabbit_token_missing_password(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='rabbit',
+            token={'username': 'rabbitmq'})
+        valid = validator.validate()
+        self.assertEqual(valid, False)
+
+    def test_auth_rabbit_token_invalid_username(self):
+        m = (auth_validate.RabbitPlainAuthTokenValidator.MAX_USERNAME_LENGTH
+             + 1)
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='rabbit',
+            token={'username': "x" * m,
+                   'password': 'rabbit'})
+        valid = validator.validate()
+        self.assertEqual(valid, False)
+
+    def test_auth_rabbit_token_invalid_password(self):
+        validator = auth_validate.AuthTokenValidator.validate_token(
+            auth_type='plain', broker_type='rabbit',
+            token={'username': 'rabbitmq',
+                   'password': ''})
+        valid = validator.validate()
+        self.assertEqual(valid, False)
