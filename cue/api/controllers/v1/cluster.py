d05666a9401102b5d0c0e891c604b5fda05a88c2
Abitha Palaniappan <abitha.palaniappan@hp.com>
2015-10-05 17:31:10 -0700

    Validate uuid parameters at api layer
    Closes-Bug:1426103
    Change-Id: I06ce0abf28dc60f70744b4e046dd20cbb27e73d3

--- cue/api/controllers/v1/cluster.py
+++ cue/api/controllers/v1/cluster.py
@@ -90,10 +90,10 @@ class Cluster(base.APIBase):
                 self.fields.append(k)
                 setattr(self, k, kwargs.get(k, wtypes.Unset))
 
-    id = wsme.wsattr(wtypes.text, readonly=True)
+    id = wsme.wsattr(wtypes.UuidType(), readonly=True)
     "UUID of cluster"
 
-    network_id = wtypes.wsattr([wtypes.text], mandatory=True)
+    network_id = wtypes.wsattr([wtypes.UuidType()], mandatory=True)
     "NIC of Neutron network"
 
     name = wsme.wsattr(wtypes.text, mandatory=True)
@@ -158,7 +158,15 @@ class ClusterController(rest.RestController):
     @wsme_pecan.wsexpose(Cluster, wtypes.text, status_code=200)
     def get_one(self, cluster_id):
         """Return this cluster."""
+
+        # validate cluster_id is of type Uuid
+        try:
+            wtypes.UuidType().validate(cluster_id)
+        except ValueError:
+            raise exception.Invalid(_("Invalid cluster ID format provided"))
+
         context = pecan.request.context
+
         cluster = get_complete_cluster(context, cluster_id)
 
         cluster.unset_empty_fields()
@@ -167,6 +175,13 @@ class ClusterController(rest.RestController):
     @wsme_pecan.wsexpose(None, wtypes.text, status_code=202)
     def delete(self, cluster_id):
         """Delete this Cluster."""
+
+        # validate cluster_id is of type Uuid
+        try:
+            wtypes.UuidType().validate(cluster_id)
+        except ValueError:
+            raise exception.Invalid(_("Invalid cluster ID format provided"))
+
         context = pecan.request.context
 
         # update cluster to deleting
