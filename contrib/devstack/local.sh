700935db3fc184816a1dae5488705fc1851a2f0e
Daniel Allegood <daniel.allegood@hp.com>
2015-10-07 10:43:30 -0700

    Code coverage fix and devstack fix
    Checkin gates are failing (both coverage and integration/tempest)
    
    Handling the case where testr fails by exiting and returning the
    exit status. Newest 'coverage' package does not support 'ignore-errors'
    in config section '[report]'.
    
    Also updating devstack plugins/scripts to use updated keystone v3
    compatible commands (openstack vs nova)
    
    Change-Id: I06d4e1179f115220608bcb15bbcfccc96769e11e

--- contrib/devstack/local.sh
+++ contrib/devstack/local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -o xtrace
+
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 source $TOP_DIR/functions
 source $TOP_DIR/stackrc
 source $TOP_DIR/lib/cue
 DEST=${DEST:-/opt/stack}
 
-source $TOP_DIR/openrc admin admin
+IDENTITY_API_VERSION=3 source $TOP_DIR/openrc admin admin
 
 IPTABLES_RULE='iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE'
 
@@ -38,8 +40,8 @@ if [[ -z $CUE_MANAGEMENT_KEY ]]; then
 fi
 
 # Add ssh keypair to admin account
-if [[ -z $(nova keypair-list | grep $CUE_MANAGEMENT_KEY) ]]; then
-    nova keypair-add --pub-key ~/.ssh/id_rsa.pub $CUE_MANAGEMENT_KEY
+if [[ -z $(openstack keypair list | grep $CUE_MANAGEMENT_KEY) ]]; then
+    openstack keypair create --public-key ~/.ssh/id_rsa.pub $CUE_MANAGEMENT_KEY
 fi
 
 # Add ping and ssh rules to rabbitmq security group
@@ -49,9 +51,22 @@ neutron security-group-rule-create --direction ingress --protocol tcp --port-ran
 # Add static nameserver to private-subnet
 neutron subnet-update --dns-nameserver 8.8.8.8 private-subnet
 
+unset OS_PROJECT_DOMAIN_ID
+unset OS_REGION_NAME
+unset OS_USER_DOMAIN_ID
+unset OS_IDENTITY_API_VERSION
+unset OS_PASSWORD
+unset OS_AUTH_URL
+unset OS_USERNAME
+unset OS_PROJECT_NAME
+unset OS_TENANT_NAME
+unset OS_VOLUME_API_VERSION
+unset COMPUTE_API_VERSION
+unset OS_NO_CACHE
+
 # Add ssh keypair to demo account
-source $TOP_DIR/openrc demo demo
-if [[ -z $(nova keypair-list | grep $CUE_MANAGEMENT_KEY) ]]; then
-    nova keypair-add --pub-key ~/.ssh/id_rsa.pub $CUE_MANAGEMENT_KEY
+IDENTITY_API_VERSION=3 source $TOP_DIR/openrc demo demo
+if [[ -z $(openstack keypair list | grep $CUE_MANAGEMENT_KEY) ]]; then
+    openstack keypair create --public-key ~/.ssh/id_rsa.pub $CUE_MANAGEMENT_KEY
 fi
 
