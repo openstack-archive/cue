3a071f00777a334b704319980fbad8ce9887e557
dagnello <dagnello@hp.com>
2015-11-11 12:50:10 -0800

    Adding support for error details on cluster creation
    Cue's current implementation does not provide any additional information
    to the user related to why a cluster went into ERROR state during
    cluster creation.  This patch adds error_description field in the
    clusters table and exposes this field through the API.  The
    update_cluster_task now extracts failure flow details and forwards this
    information on cluster record update.
    
    Partial-Bug:  1508730
    (updates also required to Cue client and dashboard)
    
    Change-Id: I440fec30bc3d051189585479b8d2c1219a98cf40

---
+++ cue/db/sqlalchemy/alembic/versions/17c428e0479e_add_cluster_error_description_column.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""add error_description column in clusters
+
+Revision ID: 17c428e0479e
+Revises: 244aa473e595
+Create Date: 2015-11-11 12:01:10.769280
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '17c428e0479e'
+down_revision = '244aa473e595'
+
+from alembic import op
+from oslo_config import cfg
+import sqlalchemy as sa
+
+
+def upgrade():
+     op.add_column('clusters', sa.Column('error_description', sa.String(
+         length=511)))
+
+
+def downgrade():
+    db_connection = cfg.CONF.database.connection
+    if db_connection != "sqlite://":  # pragma: nocover
+        op.drop_column('clusters', 'error_description')
