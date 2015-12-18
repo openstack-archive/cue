928f15fe0ec6e3a9ec541084598294b029e24bda
Daniel Allegood <daniel.allegood@hp.com>
2015-12-17 17:04:17 -0800

    Enabling devstack plugin for Cue
    Migrating our service install scripts from using the extras.d model to
    the plugin model.
    
    Change-Id: Ib6640053db1fc47a407ef1cb9e243b511ec4d960

--- tests/pre_test_hook.sh
+++ tests/pre_test_hook.sh
@@ -7,11 +7,4 @@ pushd $BASE/new/cue/contrib/devstack
 
 cp local.sh $BASE/new/devstack
 
-for f in lib/* extras.d/*; do
-    if [ ! -e "$BASE/new/devstack/$f" ]; then
-        echo "Installing: $f"
-        cp -r $f $BASE/new/devstack/$f
-    fi
-done
-
 popd
